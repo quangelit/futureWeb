8f7c3ad39d96ee7f87ed2e540de6caf60abc87ee
Quang Luong <qdl24@drexel.edu>
2018-12-14 05:55:28 -0500

    Start with create-react-app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
+import { ThemeProvider, createTheme, Arwes, Button, Paragraph } from 'arwes';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ThemeProvider theme={createTheme()}>
+        <Arwes>
+          <div style={{ margin: 20 }}>
+            <Paragraph>Hello World</Paragraph>
+          </div>
+        </Arwes>
+      </ThemeProvider>
     );
   }
 }
